c385a98aa655d844a75181e4cc420f761292f559
sandeepk27 <nanisandeep876@gmail.com>
2021-08-17 03:10:16 +0530

    hotstar

---
+++ data.js
@@ -0,0 +1,32 @@
+let movies = [{
+        name: 'Bhuj: The Pride of India',
+        des: 'Running against the clock, IAF Squadron Leader Vijay Karnik created history with determination and his love for the country. A daredevial story of guts and glory.',
+        image: 'images/slide1.png'
+    },
+    {
+        name: 'loki',
+        des: 'The mecurical villian Loki resumes his role as the God of Mischief in a new series that takes place after the "Avengers:Endgame."',
+        image: 'images/slide2.png'
+    },
+    {
+        name: 'Dance +',
+        des: 'More teams, more dance, more fun! Who impresses judge Remo and captains Punit, Dharmesh, Karishma, and Suresh in the biggest dance competition.',
+        image: 'images/slide3.png'
+    },
+    {
+        name: 'Mahabharatham',
+        des: 'Watch the rivarly of the Pandavas and Kauravas culminate in the Kurukshetra war in Mahabharatham, driven by the razor-sharp strategies of Krishna, the master puppeteer.',
+        image: 'images/slide4.png'
+    },
+    {
+        name: 'The Falcon and Winter Soldier',
+        des: 'Following the events of "Avengers:Endgame", Sam Wilson and Bucky Barnes team up in a global adventure that tests their abilities and their patience.',
+        image: 'images/slide5.png'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Wanda Maximoff and Vision - two super-powered beings living idealized suburban lives-begin to suspect that everything is not as it seems.',
+        image: 'images/slide6.png'
+    },
+    /// and so on
+]
